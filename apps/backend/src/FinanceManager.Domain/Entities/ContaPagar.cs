75c1ee43eb6f91cde661a57f3d375d0666e4dab4
Lucas Barcelos <lucavm87@gmail.com>
2025-06-22 01:49:03 -0300

    feat(domain): create the domain and entities

---
+++ apps/backend/src/FinanceManager.Domain/Entities/ContaPagar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FinanceManager.Domain.Entities
+{
+    public class ContaPagar
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public string Status { get; set; } // Ex: "Pendente", "Pago", "Atrasado"
+        public string Categoria { get; set; } // Ex: "Alimentação", "Transporte", "Saúde"
+        public DateTime CreatedAt { get; set; }
+    }
+
+
+    }   
